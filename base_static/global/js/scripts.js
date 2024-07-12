90b4f6df7d5861382e2ed2ef0f7e837bd047463d
Lucas Luza <luzalucas1999@gmail.com>
2024-07-12 15:32:37 -0300

    cover image save and rename working

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -12,3 +12,21 @@
         console.error("Logout link or form not found");
     }
 })();
+
+
+function my_scope() {
+    const forms = document.querySelectorAll('.form-delete');
+    
+    for (const form of forms) {
+      form.addEventListener('submit', function (e) {
+        e.preventDefault();
+  
+        const confirmed = confirm('Are you sure?');
+  
+        if (confirmed) {
+          form.submit();
+        }
+      });
+    }
+  }
+  my_scope();
--- products/models.py
+++ products/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
-from django.conf import settings
 
 from datetime import datetime
 from PIL import Image
@@ -9,18 +8,24 @@ from PIL import Image
 import os
 import string
 import random
-import uuid
+
+
+def generate_upload_path(instance, filename):
+    random_chars = ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
+    timestamp = datetime.now().strftime("%Y/%m/%d")
+    new_filename = f"{random_chars}.jpg"
+    return os.path.join('products', 'covers', timestamp, new_filename)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.IntegerField()
     description = models.TextField()
-    cover = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    cover = models.ImageField(upload_to=generate_upload_path, blank=True, null=True)
     is_published = models.BooleanField(default=False)
     slug = models.SlugField(unique=True, max_length=110)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
 
     def __str__(self):
         return self.name
@@ -30,13 +35,11 @@ class Product(models.Model):
             base_slug = slugify(self.name)
             random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=4))
             self.slug = f"{base_slug}-{random_string}"
-            
-        saved = super().save(*args, **kwargs)  # Salvar o objeto Product primeiro
-            
+
+        super().save(*args, **kwargs)
+
         if self.cover:
             self.resize_image(self.cover.path, 480)
-            
-        return saved  # Retornar o resultado da chamada super().save()
 
     def generate_unique_slug(self):
         base_slug = slugify(self.name)
@@ -54,26 +57,19 @@ class Product(models.Model):
     def resize_image(image_path, new_width=480):
         image_pillow = Image.open(image_path)
         original_width, original_height = image_pillow.size
-        
+
         if original_width <= new_width:
             image_pillow.close()
             return
-        
+
         new_height = round((new_width * original_height) / original_width)
-        
-        new_image = image_pillow.resize((new_width, new_height), resample=Image.LANCZOS)
-        
-        # Fechar a imagem original
+
+        new_image = image_pillow.resize((new_width, new_height), resample=Image.LANCZOS) # type: ignore
+
         image_pillow.close()
-        
-        # Salvar a nova imagem redimensionada sobrescrevendo a original
+
         new_image.save(
             image_path,
             optimize=True,
             quality=80,
         )
-        
-    def generate_filename(self):
-        random_chars = uuid.uuid4().hex[:20]
-        timestamp = datetime.now().strftime("%Y/%m/%d/%H%M%S")
-        return f"{timestamp}__{random_chars}.jpg"

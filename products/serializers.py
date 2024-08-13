5d931a0b881493fd5a737e09b3d9358729f15681
Lucas Luza <luzalucas1999@gmail.com>
2024-08-13 18:08:49 -0300

    creating a CRUD for the api

--- products/serializers.py
+++ products/serializers.py
@@ -1,22 +1,38 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from .models import Product, Category
+from users.validators import UserProductValidator
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
+    public = serializers.BooleanField(source='is_published', read_only=True)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    author_links = serializers.HyperlinkedRelatedField(
+        many=False,
+        source='author',
+        view_name='products:products_api_detail_author',
+        read_only=True
+    )
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'stock', 'price', 'description', 'public', 
+                  'category', 'author', 'author_links']
+    
+    def validate(self, attrs):
+        UserProductValidator(data=attrs, ErrorClass=serializers.ValidationError)
+        return super().validate(attrs)
+    
+    def create(self, validated_data):
+        author = validated_data.pop('author')
+        product = Product.objects.create(author=author, **validated_data)
+        return product
+    
+
+class AuthorProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=255)
     stock = serializers.IntegerField()
     price = serializers.DecimalField(max_digits=8, decimal_places=2)
-    description = serializers.CharField()
     public = serializers.BooleanField(source='is_published')
     category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
-    )
-    author_links = serializers.HyperlinkedRelatedField(
-        many=False,
-        source='author',
-        queryset=User.objects.all(),
-        view_name='products:products_api_detail_author'
-    )
     
--- products/views/api_views.py
+++ products/views/api_views.py
@@ -1,36 +1,63 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 from ..models import Product
-from ..serializers import ProductSerializer
+from ..serializers import ProductSerializer, AuthorProductSerializer
 
 
-@api_view()
+@api_view(http_method_names=['get', 'post'])
 def products_api_list(request):
-    products = Product.objects.filter(is_published=True).order_by('-created_at')[:10]
-    serializer = ProductSerializer(
-        instance=products,
-        many=True,
-        context={'request': request},
-        )
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        products = Product.objects.filter(is_published=True).order_by('-created_at')[:10]
+        serializer = ProductSerializer(
+            instance=products,
+            many=True,
+            context={'request': request},
+            )
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        product = serializer.save()
+        return Response(ProductSerializer(product, context={'request': request}).data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(http_method_names=['get', 'patch', 'delete'])
 def products_api_detail(request, pk):
     product = get_object_or_404(Product.objects.filter(is_published=True), pk=pk)
-    serializer = ProductSerializer(
-        instance=product,
-        context={'request': request},
-        )
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        serializer = ProductSerializer(
+            instance=product,
+            context={'request': request},
+            )
+        return Response(serializer.data)
+    
+    elif request.method == 'PATCH':
+        serializer = ProductSerializer(
+            instance=product,
+            data=request.data,
+            context={'request': request},
+            partial=True,
+            )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
 
 
 @api_view()
 def products_api_detail_author(request, pk):
     products = Product.objects.filter(author_id=pk, is_published=True).order_by('-created_at')
-    serializer = ProductSerializer(
+    serializer = AuthorProductSerializer(
         instance=products,
         many=True,
         context={'request': request},
